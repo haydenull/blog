d0a39287e409254083efbb82a09670893e758a40
haydenull <hayden.chen.dev@gmail.com>
2023-12-31 22:06:36 +0800

    wip: 文章卡片

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -13,7 +13,7 @@ export default function Posts() {
           在这里，你可以找到我关于编程、技术和其他主题的所有博客文章。我希望我的文章能够帮助你解决问题，启发你的思考，或者至少让你在阅读时感到愉快。我会不定期更新博客，所以请经常回来查看新的内容。如果你有任何问题或者建议，欢迎留言。
         </p>
         {/* 文章列表 */}
-        <div className="mt-16 grid gap-6 md:grid-cols-2">
+        <div className="m-auto my-16 grid max-w-[450px] gap-6 md:max-w-[800px] md:grid-cols-2">
           {blogFrontMatterList.map((frontMatter) => (
             <PostCard key={frontMatter.slug} frontMatter={frontMatter} />
           ))}
--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -2,11 +2,42 @@ import Link from 'next/link'
 
 import type { BlogFrontMatter } from '@/types/blog'
 
+import { GlowingStarsBackgroundCard } from './ui/glowing-stars'
+
 export default function PostCard({ frontMatter }: { frontMatter: BlogFrontMatter }) {
   return (
-    <Link className="rounded-lg border p-2" href={`/blog/${frontMatter.slug}`} key={frontMatter.slug}>
-      <h2 className="mt-4 text-2xl font-semibold">{frontMatter.title}</h2>
-      <p className="mt-1 text-muted-foreground">{frontMatter.description}</p>
+    <Link className="relative max-w-md overflow-hidden rounded-xl border" href={`/blog/${frontMatter.slug}`}>
+      <div className="h-64 overflow-hidden">
+        {frontMatter.cover ? (
+          <img
+            src={frontMatter.cover}
+            alt={`Cover Image for ${frontMatter.title}`}
+            className="h-full w-full object-cover"
+          />
+        ) : (
+          <GlowingStarsBackgroundCard />
+        )}
+      </div>
+      {frontMatter.cover ? (
+        <div className="absolute bottom-0 z-10 flex h-20 w-full flex-1 shrink-0 flex-col justify-between gap-0.5 bg-cover bg-bottom bg-no-repeat p-4 bg-blend-overlay [background-image:var(--post-image)] before:pointer-events-none before:absolute before:inset-0 before:z-10 before:select-none  before:bg-[--post-image-bg] before:opacity-70 before:transition-opacity after:pointer-events-none after:absolute after:inset-0 after:z-10 after:select-none  after:bg-gradient-to-b after:from-transparent after:to-[--post-image-bg] after:backdrop-blur after:transition-opacity group-hover:before:opacity-30 md:p-5">
+          <div className="absolute z-20">
+            <PostTitleAndDescription title={frontMatter.title} description={frontMatter.description} />
+          </div>
+        </div>
+      ) : (
+        <div className="absolute bottom-0 p-4">
+          <PostTitleAndDescription title={frontMatter.title} description={frontMatter.description} />
+        </div>
+      )}
     </Link>
+  )
+}
+
+function PostTitleAndDescription({ title, description }: { title: string; description?: string }) {
+  return (
+    <>
+      <h2 className="text-lg font-semibold text-white">{title}</h2>
+      <p className="mt-1 truncate text-sm text-slate-200">{description}</p>
+    </>
   )
 }
