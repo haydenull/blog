50f408ba8ea73a3731a8d051f5dd5339fe36047b
Hayden Chen <brooklyn1984@qq.com>
2024-01-04 15:08:51 +0800

    feat: 文章按日期排序

--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -9,16 +9,18 @@ const BLOG_PATH = path.join(process.cwd(), 'content/blogs')
 /** 从根目录下的 content/blogs 文件夹读取 markdown 文件，并解析 */
 export const getBlogFrontMatterList = () => {
   const blogs = fs.readdirSync(BLOG_PATH)
-  return blogs.map((blog) => {
-    const content = fs.readFileSync(path.join(process.cwd(), 'content/blogs', blog), 'utf-8')
-    const { data } = matter(content)
-    const { output: frontMatter, issues } = v.safeParse(BlogFrontMatterSchema, {
-      ...data,
-      slug: blog.replace('.md', ''),
+  return blogs
+    .map((blog) => {
+      const content = fs.readFileSync(path.join(process.cwd(), 'content/blogs', blog), 'utf-8')
+      const { data } = matter(content)
+      const { output: frontMatter, issues } = v.safeParse(BlogFrontMatterSchema, {
+        ...data,
+        slug: blog.replace('.md', ''),
+      })
+      if (issues) console.error(blog, issues)
+      return frontMatter as BlogFrontMatter
     })
-    if (issues) console.error(blog, issues)
-    return frontMatter as BlogFrontMatter
-  })
+    .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
 }
 
 /** 依据 slug 读取单个文章的数据 */
--- src/lib/weekly.ts
+++ src/lib/weekly.ts
@@ -9,16 +9,18 @@ const WEEKLY_PATH = path.join(process.cwd(), 'content/weeklies')
 /** 从根目录下的 content/weeklies 文件夹读取 markdown 文件，并解析 */
 export const getWeeklyFrontMatterList = () => {
   const weeklies = fs.readdirSync(WEEKLY_PATH)
-  return weeklies.map((weekly) => {
-    const content = fs.readFileSync(path.join(WEEKLY_PATH, weekly), 'utf-8')
-    const { data } = matter(content)
-    const { output: frontMatter, issues } = v.safeParse(BlogFrontMatterSchema, {
-      ...data,
-      slug: weekly.replace('.md', ''),
+  return weeklies
+    .map((weekly) => {
+      const content = fs.readFileSync(path.join(WEEKLY_PATH, weekly), 'utf-8')
+      const { data } = matter(content)
+      const { output: frontMatter, issues } = v.safeParse(BlogFrontMatterSchema, {
+        ...data,
+        slug: weekly.replace('.md', ''),
+      })
+      if (issues) console.error(weekly, issues)
+      return frontMatter as BlogFrontMatter
     })
-    if (issues) console.error(weekly, issues)
-    return frontMatter as BlogFrontMatter
-  })
+    .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
 }
 
 /** 依据 slug 读取单个文章的数据 */
