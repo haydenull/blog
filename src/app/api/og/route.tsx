fb40c99f36204b2be65d5267d6116cce699fdde7
Hayden Chen <brooklyn1984@qq.com>
2024-01-05 19:36:38 +0800

    chore: 优化 og image 样式

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -24,7 +24,7 @@ export async function GET(req: Request) {
   return new ImageResponse(
     (
       <div
-        tw="flex flex-col w-full h-full px-[40px] py-[40px] justify-between"
+        tw="flex flex-col w-full h-full p-[50px] justify-between bg-zinc-50"
         style={{
           backgroundImage: `url("${svgToDataUri(
             `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="#999" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`,
@@ -32,18 +32,18 @@ export async function GET(req: Request) {
         }}
       >
         <div tw="flex items-center">
-          <div tw="flex w-[60px] h-[60px] rounded-full shadow overflow-hidden mr-[20px]">
+          <div tw="flex w-[80px] h-[80px] rounded-full shadow overflow-hidden mr-[20px]">
             {/* @ts-expect-error src 的类型正确 */}
             <img tw="w-full h-full" src={avatarImg} />
           </div>
-          <span tw="text-[24px] text-zinc-700">启封 Hayden</span>
+          <span tw="text-[38px] text-zinc-700">启封 Hayden</span>
         </div>
         <div tw="flex flex-col text-left">
-          <h1 tw="text-left text-[52px] font-bold m-0">{title}</h1>
-          {description ? <p tw="text-left text-[40px] text-zinc-500 m-0">{description}</p> : null}
+          <h1 tw="text-left text-[90px] font-bold m-0">{title}</h1>
+          {description ? <p tw="text-left text-[60px] text-zinc-500 m-0">{description}</p> : null}
         </div>
         <div tw="flex justify-end">
-          <span tw="text-[18px] text-violet-500">HaydenHayden.com</span>
+          <span tw="text-[36px] text-violet-500">HaydenHayden.com</span>
         </div>
       </div>
     ),
