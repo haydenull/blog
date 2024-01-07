1549d82cdfa1d440db06a5e9f5fa23845e9f5dad
haydenull <hayden.chen.dev@gmail.com>
2024-01-07 22:30:00 +0800

    fix: code block type error

--- src/components/markdown/CodeBlock/index.tsx
+++ src/components/markdown/CodeBlock/index.tsx
@@ -18,36 +18,27 @@ const CodeBlock = ({
   return (
     <details className="code-block relative rounded bg-[#1D2021]" open>
       <summary className="cursor-pointer px-8 pb-3 pt-3 text-sm text-white">{language}</summary>
-      <div className="relative">
-        <SyntaxHighlighter
-          wrapLines
-          showLineNumbers
-          style={atomDark}
-          language={language}
-          PreTag="div"
-          className="code-container !my-0 bg-transparent !px-2 !pt-0 text-sm"
-          lineProps={(lineNumber: number) => {
-            const style = {}
-            if (highlightLines.includes(lineNumber)) {
-              style.backgroundColor = '#383e40'
-              style.width = 'fit-content'
-              style.minWidth = '100%'
-              style.display = 'block'
-            }
-            return { style }
-          }}
-          {...props}
-        >
-          {text}
-        </SyntaxHighlighter>
-        {/* <div className="highlight-line-container absolute left-0 top-0 -z-10 h-full w-full">
-          <div></div>
-          <div></div>
-          <div className="highlight-line"></div>
-          <div className="highlight-line"></div>
-          <div className="highlight-line"></div>
-        </div> */}
-      </div>
+      <SyntaxHighlighter
+        wrapLines
+        showLineNumbers
+        style={atomDark}
+        language={language}
+        PreTag="div"
+        className="code-container !my-0 bg-transparent !px-2 !pt-0 text-sm"
+        lineProps={(lineNumber: number) => {
+          const style: Record<string, unknown> = {}
+          if (highlightLines.includes(lineNumber)) {
+            style.backgroundColor = '#383e40'
+            style.width = 'fit-content'
+            style.minWidth = '100%'
+            style.display = 'block'
+          }
+          return { style }
+        }}
+        {...props}
+      >
+        {text}
+      </SyntaxHighlighter>
       <CopyButton text={text} />
     </details>
   )
