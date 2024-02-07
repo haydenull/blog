7893fe6fcfae9a07a3190daed5b1f23fb0b44733
haydenull <hayden.chen.dev@gmail.com>
2024-02-07 08:33:31 +0800

    wip: 重构周刊列表样式

--- src/app/weekly/page.tsx
+++ src/app/weekly/page.tsx
@@ -13,7 +13,7 @@ export default function Posts() {
           欢迎来到十五周刊，我们每周都会分享有价值的前端技术文章，让你随时掌握前端的最新动态。此外，我们还会介绍一些实用的软件工具，帮助你提升工作效率。同时，我们也会分享一些个人生活感悟，希望通过分享，能够给你的生活带来一些启示和思考。希望你会喜欢我们的内容。
         </p>
         {/* 文章列表 */}
-        <div className="m-auto my-16 grid max-w-[450px] gap-6 md:max-w-[800px] md:grid-cols-2">
+        <div className="m-auto my-16 flex max-w-[450px] flex-col items-center divide-y md:max-w-[800px]">
           {weeklyFrontMatterList.map((frontMatter) => (
             <WeeklyCard key={frontMatter.slug} frontMatter={frontMatter} />
           ))}
--- src/components/WeeklyCard.tsx
+++ src/components/WeeklyCard.tsx
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs'
 import Link from 'next/link'
 
 import type { BlogFrontMatter } from '@/types/blog'
@@ -5,9 +6,18 @@ import type { BlogFrontMatter } from '@/types/blog'
 import { GlowingStarsBackgroundCard } from './ui/glowing-stars'
 
 export default function WeeklyCard({ frontMatter }: { frontMatter: BlogFrontMatter }) {
+  const dateString = dayjs(frontMatter.date).format('YYYY/MM/DD')
+
   return (
-    <Link className="relative max-w-md overflow-hidden rounded-xl border" href={`/weekly/${frontMatter.slug}`}>
-      <div className="relative h-[200px] overflow-hidden">
+    <Link className="relative flex max-w-xl gap-4 overflow-hidden py-4 md:gap-4" href={`/weekly/${frontMatter.slug}`}>
+      <div className="flex flex-1 flex-col justify-between">
+        <div>
+          <h2 className="text-lg font-semibold text-foreground md:text-xl">{frontMatter.title}</h2>
+          <p className="mt-0.5 text-sm text-muted-foreground">{frontMatter.description}</p>
+        </div>
+        <time className="text-xs text-zinc-400">{dateString}</time>
+      </div>
+      <div className="relative h-[80px] w-[142px] overflow-hidden rounded-md md:h-[120px] md:w-[214px]">
         {frontMatter.cover ? (
           <>
             <img
@@ -20,10 +30,6 @@ export default function WeeklyCard({ frontMatter }: { frontMatter: BlogFrontMatt
         ) : (
           <GlowingStarsBackgroundCard />
         )}
-      </div>
-      <div className="px-2 pb-2 pt-4">
-        <h2 className="text-xl font-semibold text-foreground">{frontMatter.title}</h2>
-        <p className="mt-1 text-sm text-muted-foreground">{frontMatter.description}</p>
       </div>
     </Link>
   )
