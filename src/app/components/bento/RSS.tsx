18dadd9cd9530dbc8d35ef9f91a0a83c6d0a2df6
Hayden Chen <hayden.chen.dev@gmail.com>
2024-04-19 16:14:12 +0800

    chore: :wrench: 优化 rss 样式

--- src/app/components/bento/RSS.tsx
+++ src/app/components/bento/RSS.tsx
@@ -4,11 +4,14 @@ import BentoCard from './BentoCard'
 
 const RSS = () => {
   return (
-    <BentoCard url="/feed.xml" className="group flex items-center justify-center">
+    <BentoCard
+      url="/feed.xml"
+      className="group flex items-center justify-center bg-gradient-to-br from-orange-100 to-orange-200 dark:from-orange-900 dark:to-orange-950"
+    >
       <div className="relative flex h-[50px] w-[50px] items-center justify-center">
         <div className="absolute h-full w-full rounded-md bg-orange-400 transition-all duration-300 group-hover:scale-[10] dark:bg-orange-600"></div>
         <div>
-          <StreamlineRssSymbolSolid className="relative text-3xl text-white transition-all group-hover:text-6xl dark:text-zinc-200" />
+          <StreamlineRssSymbolSolid className="relative text-3xl text-white transition-all duration-100 group-hover:text-6xl dark:text-zinc-200" />
           <p className="relative text-[0px] text-white transition-all delay-100 duration-100 group-hover:text-base dark:text-zinc-200">
             RSS
           </p>
