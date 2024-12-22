d66f2a66c78449e8f6993d681edb27b134113878
nichuanfang <f18326186224@gmail.com>
2024-12-23 00:31:55 +0800

    feat: auto switch theme default

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,22 +1,8 @@
 'use client'
 
-import { ThemeProvider as NextThemesProvider, useTheme } from 'next-themes'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { type ThemeProviderProps } from 'next-themes/dist/types'
-import { useEffect } from 'react'
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  const { setTheme, theme } = useTheme()
-
-  useEffect(() => {
-    if (!theme) {
-      const hour = new Date().getHours()
-      if (hour >= 6 && hour < 18) {
-        setTheme('light')
-      } else {
-        setTheme('dark')
-      }
-    }
-  }, [theme, setTheme])
-
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
